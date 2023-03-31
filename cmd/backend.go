8bafd49ddc5adecea07f3266207a2860df896197
Ricky Whitaker <ricky.whitaker@grafana.com>
2023-03-31 14:02:21 -0500

    added cli flags to cmd backend test function

--- cmd/backend.go
+++ cmd/backend.go
@@ -1,18 +1,124 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/grafana/grafana-build/pipelines"
 	"github.com/urfave/cli/v2"
+	"strings"
 )
 
-var TestBackendUnit = &cli.Command{
-	Name:   "test",
-	Action: PipelineAction(pipelines.GrafanaBackendTests),
+type ChoiceFlag struct {
+	Name         string
+	Usage        string
+	Value        string
+	HasBeenSet   bool
+	Choices      []string
+	Aliases      []string
+	defaultValue string
+	Category     string
+	DefaultText  string
+	Required     bool
+	Hidden       bool
+	EnvVars      []string
 }
 
-var TestBackendIntegration = &cli.Command{
-	Name:   "test-integration",
-	Action: PipelineAction(pipelines.GrafanaBackendTestIntegration),
+func (f *ChoiceFlag) String() string {
+	return cli.FlagStringer(f)
+}
+
+func (f *ChoiceFlag) Apply(set *flag.FlagSet) error {
+	// set default value so that environment wont be able to overwrite it
+	f.defaultValue = f.Value
+
+	for _, name := range f.Names() {
+		set.String(name, f.Value, f.Usage)
+	}
+
+	return nil
+}
+
+// TakesValue returns true of the flag takes a value, otherwise false
+func (f *ChoiceFlag) TakesValue() bool {
+	return false
+}
+
+// Names returns the names of the flag
+func (f *ChoiceFlag) Names() []string {
+	return cli.FlagNames(f.Name, f.Aliases)
+}
+
+func (f *ChoiceFlag) IsSet() bool {
+	return f.HasBeenSet
+}
+
+// IsRequired returns whether or not the flag is required
+func (f *ChoiceFlag) IsRequired() bool {
+	return f.Required
+}
+
+// IsVisible returns true if the flag is not hidden, otherwise false
+func (f *ChoiceFlag) IsVisible() bool {
+	return !f.Hidden // this function is required for help text to show
+}
+
+// GetDefaultText returns the default text for this flag
+func (f *ChoiceFlag) GetDefaultText() string {
+	if f.DefaultText != "" {
+		return f.DefaultText
+	}
+	return fmt.Sprintf("%v", f.defaultValue)
+}
+
+// GetUsage returns the usage string for the flag
+func (f *ChoiceFlag) GetUsage() string {
+	return fmt.Sprintf("%s (choices: [%s])", f.Usage, strings.Join(f.Choices, ","))
+}
+
+// GetCategory returns the category for the flag
+func (f *ChoiceFlag) GetCategory() string {
+	return f.Category
+}
+
+// GetValue returns the flags value as string representation and an empty
+// string if the flag takes no value at all.
+func (f *ChoiceFlag) GetValue() string {
+	return f.Value
+}
+
+// GetEnvVars returns the env vars for this flag
+func (f *ChoiceFlag) GetEnvVars() []string {
+	return f.EnvVars
+}
+
+var FlagUnit = &cli.BoolFlag{
+	Name:  "unit",
+	Usage: "Run the backend unit tests",
+	Value: true,
+}
+
+var FlagIntegration = &cli.BoolFlag{
+	Name:  "integration",
+	Usage: "Run the backend integration tests",
+	Value: false,
+}
+
+// FlagDatabase is a multichoice Flag
+var FlagDatabase = &ChoiceFlag{
+	Name:    "database",
+	Usage:   "Which database to use, only valid when --integration=true",
+	Choices: pipelines.IntegrationDatabases,
+	Value:   "sqlite",
+}
+
+var TestBackend = &cli.Command{
+	Name:   "test",
+	Action: PipelineAction(pipelines.GrafanaBackendTests),
+	Flags: []cli.Flag{
+		FlagUnit,
+		FlagIntegration,
+		FlagDatabase,
+	},
 }
 
 var BuildBackend = &cli.Command{
@@ -23,4 +129,4 @@ var BuildBackend = &cli.Command{
 	},
 }
 
-var BackendCommands = []*cli.Command{TestBackendUnit, TestBackendIntegration, BuildBackend}
+var BackendCommands = []*cli.Command{TestBackend, BuildBackend}
--- pipelines/backend_tests.go
+++ pipelines/backend_tests.go
@@ -2,21 +2,49 @@ package pipelines
 
 import (
 	"context"
-
 	"dagger.io/dagger"
+	"fmt"
 	"github.com/grafana/grafana-build/containers"
+	"log"
+	"strings"
 )
+
+var IntegrationDatabases = []string{"sqlite", "mysql", "postgres"}
+
+// list of strings, string
+func Contains(arr []string, v string) bool {
+	for _, str := range arr {
+		if str == v {
+			return true
+		}
+	}
+	return false
+}
 
 // GrafanabackendTests runs the Grafana backend test containers for short (unit) and integration tests.
 func GrafanaBackendTests(ctx context.Context, d *dagger.Client, args PipelineArgs) error {
+
+	db := args.Context.String("database")
+	log.Println("db", db)
+
+	// verify the database is one of the available choices
+	if !Contains(IntegrationDatabases, db) {
+		return fmt.Errorf("database needs to be one of %s", strings.Join(IntegrationDatabases, ","))
+	}
+	var r = []*dagger.Container{}
 	c := d.Pipeline("backend tests", dagger.PipelineOpts{
 		Description: "Runs backend unit tests",
 	})
-
-	r := []*dagger.Container{
-		containers.BackendTestShort(c, args.Grafana),
+	if args.Context.Bool("unit") == true {
+		// add unit tests to execution list
+		log.Println("Unit tests will be run")
+		r = append(r, containers.BackendTestShort(c, args.Grafana))
 	}
-
+	if args.Context.Bool("integration") == true {
+		// add integration tests to execution list
+		log.Printf("Integration tests will be run using a '%s' database", db)
+		r = append(r, containers.BackendTestIntegration(c, args.Grafana))
+	}
 	return containers.Run(ctx, r)
 }
 
