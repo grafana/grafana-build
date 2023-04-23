583d88430ed1aae46a79b9de24c0a7ffcec52974
Kevin Minehart <kmineh0151@gmail.com>
2023-04-23 09:19:55 -0500

    add publish script

---
+++ actions/package/publish/publish.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+go run ./cmd \
+  --build-id=${DRONE_BUILD_ID} \
+  --grafana-dir=${GRAFANA_DIR} \
+  --github-token=${GITHUB_TOKEN} \
+  package \
+  --distro=${DISTROS} \
+  publish \
+  --destination=${DESTINATION}/${DRONE_BUILD_EVENT} \
+  --gcp-service-account-key-base64=${GCP_KEY_BASE64}
