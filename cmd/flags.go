47b4be9f42130877c250cfcf52df60b5111d7053
Horst Gutmann <horst.gutmann@grafana.com>
2023-04-25 15:12:31 +0200

    Add version flag to explicitly set Grafana version string

--- cmd/flags.go
+++ cmd/flags.go
@@ -9,3 +9,8 @@ var FlagDistros = &cli.StringSliceFlag{
 	Usage: "See the list of distributions with 'go tool dist list'. For variations of the same distribution, like 'armv6' or 'armv7', append an extra path part. Example: 'linux/arm/v6', or 'linux/amd64/v3'.",
 	Value: cli.NewStringSlice(DefaultDistros...),
 }
+
+var FlagVersion = &cli.StringFlag{
+	Name:  "version",
+	Usage: "Explicit version number. If this is not set then one with will auto-detected based on the source repository",
+}
