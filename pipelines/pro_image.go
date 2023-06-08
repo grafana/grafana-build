87f30446e9c2a777e929f9d4bc07b2aa9fc2ede2
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-06-08 17:01:46 -0300

    pro-image: return error if container exit code is different than 0

--- pipelines/pro_image.go
+++ pipelines/pro_image.go
@@ -22,9 +22,6 @@ func ProImage(ctx context.Context, dc *dagger.Client, directory *dagger.Director
 	container := dc.Container().From("golang:1.20-alpine").
 		WithUnixSocket(socketPath, socket).
 		WithExec([]string{"apk", "add", "--update", "make", "docker", "git", "jq", "jsonnet"}).
-		// WithExec([]string{"curl", "-O", "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-434.0.0-linux-x86_64.tar.gz"}).
-		// WithExec([]string{"tar", "-xf", "google-cloud-cli-434.0.0-linux-x86_64.tar.gz", "--strip-components=1", "-C", "/"}).
-		// WithExec([]string{"/install.sh", "--disable-prompts"}).
 		WithDirectory("/src", hostedGrafanaRepo).
 		WithFile("/src/grafana.deb", debianPackageFile, dagger.ContainerWithFileOpts{}).
 		WithWorkdir("/src").
@@ -34,17 +31,32 @@ func ProImage(ctx context.Context, dc *dagger.Client, directory *dagger.Director
 		}).
 		WithExec([]string{
 			"/bin/sh", "-c",
-			fmt.Sprintf("docker build --platform=linux/amd64 --build-arg=RELEASE_TYPE=%s --build-arg=GRAFANA_VERSION=%s --build-arg=HGRUN_IMAGE=hgrun:latest . -f ./docker/hosted-grafana-all/Dockerfile -t hostedgrafana:latest", args.ProImageOpts.ReleaseType, args.ProImageOpts.GrafanaVersion),
+			fmt.Sprintf("docker build --platform=linux/amd64 --build-arg=RELEASE_TYPE=%s --build-arg=GRAFANA_VERSION=%s --build-arg=HGRUN_IMAGE=hgrun:latest . -f ./docker/hosted-grafana-all/Dockerfile -t hostedgrafana:latest",
+				args.ProImageOpts.ReleaseType,
+				args.ProImageOpts.GrafanaVersion,
+			),
 		})
 
 	if args.ProImageOpts.Push {
 		panic("TODO: push to container registry")
 	}
-	stdout, err := container.Stdout(context.Background())
-	fmt.Printf("\n\naaaaaaa stdout=%+v err=%+v\n\n", stdout, err)
 
-	stderr, err := container.Stderr(context.Background())
-	fmt.Printf("\n\naaaaaaa stderr=%+v err=%+v\n\n", stderr, err)
+	exitCode, err := container.ExitCode(ctx)
+	if err != nil {
+		return fmt.Errorf("getting container exit code: %w", err)
+	}
+
+	if exitCode != 0 {
+		stderr, err := container.Stderr(ctx)
+		if err != nil {
+			return fmt.Errorf("getting container stderr: exitCode=%d %w", exitCode, err)
+		}
+		stdout, err := container.Stdout(ctx)
+		if err != nil {
+			return fmt.Errorf("getting container stdout: exitCode=%d %w", exitCode, err)
+		}
+		return fmt.Errorf("container exit code is not 0: exitCode=%d stderr=%s stdout=%s", exitCode, stderr, stdout)
+	}
 
 	return nil
 }
