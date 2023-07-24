024f997c27f4edd5a2d4363201c139c911d191d0
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-07-24 09:49:39 -0300

    Add dir and ref to enterprise and pro scripts

--- scripts/drone_publish_tag_pro.sh
+++ scripts/drone_publish_tag_pro.sh
@@ -19,7 +19,8 @@ go run ./cmd \
   --enterprise \
   --grafana=false \
   --build-id=${DRONE_BUILD_NUMBER} \
-  --grafana-dir=${GRAFANA_DIR} \
+  --enterprise-dir=${GRAFANA_DIR} \
+  --grafana-ref=${DRONE_TAG} \
   --github-token=${GITHUB_TOKEN} \
   --version=${DRONE_TAG} \
   --destination=${local_dst} \
