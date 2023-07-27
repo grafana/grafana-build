84d3f1cd753534123b1b6a8179491685726a1727
Guilherme Caulada <guilherme.caulada@gmail.com>
2023-07-27 13:47:11 -0300

    Fix description comment on storybook container
    Co-authored-by: Kevin Minehart <kmineh0151@gmail.com>

--- containers/storybook.go
+++ containers/storybook.go
@@ -4,7 +4,7 @@ import (
 	"dagger.io/dagger"
 )
 
-// Storybook returns a dagger.Directory which contains the Grafana NPM packages from the grafana source code.
+// Storybook returns a dagger.Directory which contains the built storybook server.
 func Storybook(d *dagger.Client, src *dagger.Directory, opts *YarnCacheOpts, version, nodeVersion string) *dagger.Directory {
 	c := NodeContainer(d, NodeImage(nodeVersion)).
 		WithMountedDirectory("/src", src).
