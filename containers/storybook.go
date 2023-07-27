a03df3034b919f32dca42dbd77f531363881fb35
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-07-27 13:13:34 -0300

    Add storybook to packages

---
+++ containers/storybook.go
@@ -0,0 +1,19 @@
+package containers
+
+import (
+	"dagger.io/dagger"
+)
+
+// Storybook returns a dagger.Directory which contains the Grafana NPM packages from the grafana source code.
+func Storybook(d *dagger.Client, src *dagger.Directory, opts *YarnCacheOpts, version, nodeVersion string) *dagger.Directory {
+	c := NodeContainer(d, NodeImage(nodeVersion)).
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src")
+
+	c = WithYarnCache(c, opts)
+
+	c = c.WithExec([]string{"yarn", "install", "--immutable"}).
+		WithExec([]string{"yarn", "run", "storybook:build"})
+
+	return c.Directory("./packages/grafana-ui/dist/storybook")
+}
