d4e80c0421282360216632741a90a454914f5a31
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-08-14 18:01:36 -0300

    Add package publish command

--- pipelines/package_publish.go
+++ pipelines/package_publish.go
@@ -2,99 +2,35 @@ package pipelines
 
 import (
 	"context"
-	"log"
+	"path/filepath"
 	"strings"
 
 	"dagger.io/dagger"
 	"github.com/grafana/grafana-build/containers"
-	"github.com/grafana/grafana-build/slices"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/sync/semaphore"
 )
 
-// PublishPackage creates a package and publishes it to a Google Cloud Storage bucket.
-func PublishPackage(ctx context.Context, d *dagger.Client, src *dagger.Directory, args PipelineArgs) error {
+// PublishPackage takes one or multiple grafana.tar.gz as input and publishes it to a set destination.
+func PublishPackage(ctx context.Context, d *dagger.Client, args PipelineArgs) error {
 	var (
-		opts = args.GrafanaOpts
-		// This bool slice stores the values of args.BuildEnterprise for each build
-		// --enterprise: []bool{true}
-		// --enterprise --grafana: []bool{true, false}
-		// if -- enterprise is not used it always returns []bool{false}
-		skipOss           = opts.BuildEnterprise && !opts.BuildGrafana
-		isEnterpriseBuild = slices.Unique([]bool{opts.BuildEnterprise, skipOss})
-		distros           = args.PackageOpts.Distros
-		nodeCache         = d.CacheVolume("yarn-dependencies")
+		wg = &errgroup.Group{}
+		sm = semaphore.NewWeighted(args.ConcurrencyOpts.Parallel)
 	)
 
-	files := map[string]*dagger.File{}
-
-	for _, isEnterprise := range isEnterpriseBuild {
-		var (
-			src     = src
-			edition = ""
-		)
-		if isEnterprise {
-			edition = "enterprise"
-			// If the user has manually set the edition flag, then override it with their selection.
-			// Temporary fix: to avoid creating a --grafana and a --enterprise package with a conflicting name, only allow
-			// overriding the 'edition' if we're building enterprise.
-			if e := args.PackageOpts.Edition; e != "" {
-				edition = e
-			}
-			s, err := args.GrafanaOpts.Enterprise(ctx, src, d)
-			if err != nil {
-				return err
-			}
-			src = s
-		}
-
-		opts := PackageOpts{
-			GrafanaCompileOpts: &GrafanaCompileOpts{
-				Source:           src,
-				Version:          opts.Version,
-				Platform:         args.Platform,
-				Env:              args.GrafanaOpts.Env,
-				GoTags:           args.GrafanaOpts.GoTags,
-				Edition:          edition,
-				YarnCacheHostDir: args.GrafanaOpts.YarnCacheHostDir,
-			},
-			BuildID:         opts.BuildID,
-			Distributions:   distros,
-			Edition:         edition,
-			NodeCacheVolume: nodeCache,
-		}
-		packages, err := PackageFiles(ctx, d, opts)
-		if err != nil {
-			return err
-		}
-
-		for distro, targz := range packages {
-			opts := TarFileOpts{
-				Edition: opts.Edition,
-				Version: opts.Version,
-				BuildID: opts.BuildID,
-				Distro:  distro,
-			}
-
-			fn := TarFilename(opts)
-			dst := strings.Join([]string{args.PublishOpts.Destination, fn}, "/")
-			log.Println("Writing package", fn, "to", dst)
-			files[dst] = targz
-		}
+	packages, err := containers.GetPackages(ctx, d, args.PackageInputOpts, args.GCPOpts)
+	if err != nil {
+		return err
 	}
-	var (
-		grp = &errgroup.Group{}
-		sm  = semaphore.NewWeighted(args.ConcurrencyOpts.Parallel)
-	)
 
-	for dst, file := range files {
-		grp.Go(PublishFileFunc(ctx, sm, d, &containers.PublishFileOpts{
-			File:        file,
-			Destination: dst,
+	for i, name := range args.PackageInputOpts.Packages {
+		wg.Go(PublishFileFunc(ctx, sm, d, &containers.PublishFileOpts{
+			File:        packages[i],
+			Destination: strings.Join([]string{args.PublishOpts.Destination, filepath.Base(name)}, "/"),
 			PublishOpts: args.PublishOpts,
 			GCPOpts:     args.GCPOpts,
 		}))
 	}
 
-	return grp.Wait()
+	return wg.Wait()
 }
