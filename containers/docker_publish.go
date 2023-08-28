2c2e2c1f1f703b56d930a41216f53176cd1ea586
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-08-28 16:40:23 -0300

    Fix typo on docker environment variable

--- containers/docker_publish.go
+++ containers/docker_publish.go
@@ -14,7 +14,7 @@ func PublishPackageImage(ctx context.Context, d *dagger.Client, pkg *dagger.File
 	return d.Container().From("docker").
 		WithFile("grafana.img", pkg).
 		WithSecretVariable("DOCKER_USERNAME", username).
-		WithSecretVariable("DOCKER_PASSWORD0", password).
+		WithSecretVariable("DOCKER_PASSWORD", password).
 		WithUnixSocket("/var/run/docker.sock", d.Host().UnixSocket("/var/run/docker.sock")).
 		WithExec([]string{"/bin/sh", "-c", fmt.Sprintf("docker login %s -u $DOCKER_USERNAME -p $DOCKER_PASSWORD", opts.Registry)}).
 		WithExec([]string{"/bin/sh", "-c", "docker load -i grafana.img | awk -F 'Loaded image: ' '{print $2}' > /tmp/image_tag"}).
@@ -30,7 +30,7 @@ func PublishDockerManifest(ctx context.Context, d *dagger.Client, manifest strin
 	return d.Container().From("docker").
 		WithUnixSocket("/var/run/docker.sock", d.Host().UnixSocket("/var/run/docker.sock")).
 		WithSecretVariable("DOCKER_USERNAME", username).
-		WithSecretVariable("DOCKER_PASSWORD0", password).
+		WithSecretVariable("DOCKER_PASSWORD", password).
 		WithExec([]string{"/bin/sh", "-c", fmt.Sprintf("docker login %s -u $DOCKER_USERNAME -p $DOCKER_PASSWORD", opts.Registry)}).
 		WithExec(append([]string{"docker", "manifest", "create", manifest}, tags...)).
 		WithExec([]string{"docker", "manifest", "push", manifest}).
