5e810113948532f097e419ce3834b0d1934a7c28
Kevin Minehart <kmineh0151@gmail.com>
2023-08-28 09:14:54 -0500

    dagger run -> dagger run --silent

--- scripts/drone_publish_tag_enterprise.sh
+++ scripts/drone_publish_tag_enterprise.sh
@@ -32,7 +32,7 @@ echo "Done building tar.gz packages..."
 
 # Use the non-windows, non-darwin packages and create deb packages from them.
 dagger run --silent go run ./cmd deb \
-  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | awk '{print "--package=" $0}') \
+  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | grep -v arm-6 | awk '{print "--package=" $0}') \
   --parallel=16 \
   --checksum \
   --destination=${local_dst} \
@@ -40,7 +40,7 @@ dagger run --silent go run ./cmd deb \
 
 # Make rpm installers for all the same Linux distros, and sign them because RPM packages are signed.
 dagger run --silent go run ./cmd rpm \
-  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | awk '{print "--package=" $0}') \
+  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | grep -v arm-6 | awk '{print "--package=" $0}') \
   --parallel=16 \
   --checksum \
   --destination=${local_dst} \
