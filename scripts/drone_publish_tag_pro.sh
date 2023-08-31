cd4026e0d6a5a6fe094bf51555b9f9965000ac0f
Kevin Minehart <kmineh0151@gmail.com>
2023-08-31 09:19:54 -0500

    Fix pro debs

--- scripts/drone_publish_tag_pro.sh
+++ scripts/drone_publish_tag_pro.sh
@@ -6,44 +6,44 @@ set -e
 # This command enables qemu emulators for building Docker images for arm64/armv6/armv7/etc on the host.
 docker run --privileged --rm tonistiigi/binfmt --install all
 
-# Build all of the grafana.tar.gz packages.
-dagger run --silent go run ./cmd \
-  package \
-  --yarn-cache=${YARN_CACHE_FOLDER} \
-  --distro=linux/amd64 \
-  --distro=linux/arm64 \
-  --distro=linux/arm/v6 \
-  --distro=linux/arm/v7 \
-  --distro=darwin/amd64 \
-  --distro=windows/amd64 \
-  --env GO_BUILD_TAGS=pro \
-  --env WIRE_TAGS=pro \
-  --go-tags=pro \
-  --edition=pro \
-  --checksum \
-  --enterprise \
-  --grafana=false \
-  --build-id=${DRONE_BUILD_NUMBER} \
-  --enterprise-ref=${DRONE_TAG} \
-  --grafana-ref=${DRONE_TAG} \
-  --grafana-repo=https://github.com/grafana/grafana-security-mirror.git \
-  --github-token=${GITHUB_TOKEN} \
-  --version=${DRONE_TAG} \
-  --destination=${local_dst} \
-  --gcp-service-account-key-base64=${GCP_KEY_BASE64} > assets.txt
+# # Build all of the grafana.tar.gz packages.
+# dagger run --silent go run ./cmd \
+#   package \
+#   --yarn-cache=${YARN_CACHE_FOLDER} \
+#   --distro=linux/amd64 \
+#   --distro=linux/arm64 \
+#   --distro=linux/arm/v6 \
+#   --distro=linux/arm/v7 \
+#   --distro=darwin/amd64 \
+#   --distro=windows/amd64 \
+#   --env GO_BUILD_TAGS=pro \
+#   --env WIRE_TAGS=pro \
+#   --go-tags=pro \
+#   --edition=pro \
+#   --checksum \
+#   --enterprise \
+#   --grafana=false \
+#   --build-id=${DRONE_BUILD_NUMBER} \
+#   --enterprise-ref=${DRONE_TAG} \
+#   --grafana-ref=${DRONE_TAG} \
+#   --grafana-repo=https://github.com/grafana/grafana-security-mirror.git \
+#   --github-token=${GITHUB_TOKEN} \
+#   --version=${DRONE_TAG} \
+#   --destination=${local_dst} \
+#   --gcp-service-account-key-base64=${GCP_KEY_BASE64} > assets.txt
 
 echo "Done building tar.gz packages..."
 
 # Use the .tar.gz packages and create deb packages from them.
 dagger run --silent go run ./cmd deb \
-  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | awk '{print "--package=" $0}') \
+  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | grep -v arm-6 | awk '{print "--package=" $0}') \
   --checksum \
   --destination=${local_dst} \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} > debs.txt &
 
 # Build a docker image for all .tar.gz packages
 dagger run --silent go run ./cmd docker \
-  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | awk '{print "--package=" $0}') \
+  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | grep -v windows | grep -v darwin | grep -v arm-6 | awk '{print "--package=" $0}') \
   --checksum \
   --ubuntu-base="ubuntu:22.10" \
   --alpine-base="alpine:3.18.0" \
@@ -52,7 +52,7 @@ dagger run --silent go run ./cmd docker \
 
 # Copy only the linux/amd64 edition frontends into a separate folder
 dagger run --silent go run ./cmd cdn \
-  $(cat assets.txt | grep tar.gz | grep -v docker | grep -v sha256 | awk '{print "--package=" $0}') \
+  $(cat assets.txt | grep tar.gz | grep amd64 | grep linux | grep -v docker | grep -v sha256 | awk '{print "--package=" $0}') \
   --destination=${local_dst} \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} > cdn.txt &
 
