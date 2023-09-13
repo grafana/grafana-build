9dd0f5a646472f2fb3715cd03b05564b28b31f82
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-09-13 16:17:50 -0300

    Fix storybook and cdn destinations

--- scripts/drone_publish_nightly_grafana.sh
+++ scripts/drone_publish_nightly_grafana.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+ver="nightly-${DRONE_COMMIT_SHA:0:8}"
 local_dir="${DRONE_WORKSPACE}/dist"
 
 # Publish the docker images present in the bucket
@@ -13,10 +14,10 @@ dagger run --silent go run ./cmd docker publish \
 dagger run --silent go run ./cmd storybook \
   $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | awk '{print "--package=file://"$0}') \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} \
-  --destination=${STORYBOOK_DESTINATION} 
+  --destination="${STORYBOOK_DESTINATION}/${ver}" 
 
 # Copy only the linux/amd64 edition static assets into a separate folder
 dagger run --silent go run ./cmd cdn \
   $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | awk '{print "--package=file://"$0}') \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} \
-  --destination=${CDN_DESTINATION} 
+  --destination="${CDN_DESTINATION}/${ver}/public" 
