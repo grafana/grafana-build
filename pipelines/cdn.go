c6a1e3d015d6687573b54dfe200328066e56f827
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-09-13 16:09:55 -0300

    Simplify extraction commands destination path

--- pipelines/cdn.go
+++ pipelines/cdn.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"dagger.io/dagger"
 	"github.com/grafana/grafana-build/containers"
@@ -24,14 +23,10 @@ func CDN(ctx context.Context, d *dagger.Client, args PipelineArgs) error {
 			targz = packages[i]
 		)
 
-		// We can't use path.Join here because publishopts.Destination might have a URL scheme which will get santizied, and we can't use filepath.Join because Windows would use \\ filepath separators.
-		dst := strings.Join([]string{args.PublishOpts.Destination, name, "public"}, "/")
+		log.Println("Copying frontend assets for", name, "to", args.PublishOpts.Destination)
 
-		log.Println("Copying frontend assets for", name, "to", dst)
-
-		// gcloud rsync the public folder to ['dst']/public
 		public := containers.ExtractedArchive(d, targz, name).Directory("public")
-		dst, err := containers.PublishDirectory(ctx, d, public, args.GCPOpts, dst)
+		dst, err := containers.PublishDirectory(ctx, d, public, args.GCPOpts, args.PublishOpts.Destination)
 		if err != nil {
 			return err
 		}
--- pipelines/storybook.go
+++ pipelines/storybook.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"dagger.io/dagger"
 	"github.com/grafana/grafana-build/containers"
@@ -24,14 +23,10 @@ func Storybook(ctx context.Context, d *dagger.Client, args PipelineArgs) error {
 			targz = packages[i]
 		)
 
-		// We can't use path.Join here because publishopts.Destination might have a URL scheme which will get santizied, and we can't use filepath.Join because Windows would use \\ filepath separators.
-		dst := strings.Join([]string{args.PublishOpts.Destination, name, "storybook"}, "/")
+		log.Println("Copying storybook assets for", name, "to", args.PublishOpts.Destination)
 
-		log.Println("Copying storybook assets for", name, "to", dst)
-
-		// gcloud rsync the storybook folder to ['dst']/storybook
 		storybook := containers.ExtractedArchive(d, targz, name).Directory("storybook")
-		dst, err := containers.PublishDirectory(ctx, d, storybook, args.GCPOpts, dst)
+		dst, err := containers.PublishDirectory(ctx, d, storybook, args.GCPOpts, args.PublishOpts.Destination)
 		if err != nil {
 			return err
 		}
