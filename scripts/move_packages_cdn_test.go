18a26eeb07fb9b2d200d35f4cb7d4b14d04ae0d9
Kevin Minehart <kmineh0151@gmail.com>
2023-09-14 08:18:46 -0500

    fix docker sha256 copying (#167)
    Co-authored-by: Horst Gutmann <horst.gutmann@grafana.com>

--- scripts/move_packages_cdn_test.go
+++ scripts/move_packages_cdn_test.go
@@ -1,50 +1,23 @@
 package main
 
-import (
-	"sort"
-	"testing"
-)
-
-var cdnMapping = []m{
-	{
+var cdnMapping = map[string]m{
+	"OSS: Linux AMD64": {
 		input: "gs://bucket/tag/grafana_v1.2.3_102_linux_amd64/public",
 		output: []string{
 			"artifacts/static-assets/grafana-oss/1.2.3/public",
 			"artifacts/static-assets/grafana/1.2.3/public",
 		},
 	},
-	{
+	"ENT: Linux AMD64": {
 		input: "gs://bucket/tag/grafana-enterprise_v1.2.3_102_linux_amd64/public",
 		output: []string{
 			"artifacts/static-assets/grafana-enterprise/1.2.3/public",
 		},
 	},
-	{
+	"PRO: Linux AMD64": {
 		input: "gs://bucket/tag/grafana-pro_v1.2.3_102_linux_amd64/public",
 		output: []string{
 			"artifacts/static-assets/grafana-pro/1.2.3/public",
 		},
 	},
-}
-
-func TestMoveCDN(t *testing.T) {
-	for _, v := range cdnMapping {
-		out := CDNHandler(v.input)
-
-		if len(out) != len(v.output) {
-			t.Errorf("expected %d in output but received %d\nexpected: %v\nreceived: %v", len(v.output), len(out), v.output, out)
-			continue
-		}
-		sort.Strings(out)
-		exp := v.output
-		sort.Strings(exp)
-
-		for i := range out {
-			got := out[i]
-			expect := exp[i]
-			if expect != got {
-				t.Errorf("\nExpected %s\nReceived %s", expect, got)
-			}
-		}
-	}
 }
--- scripts/move_packages_npm_test.go
+++ scripts/move_packages_npm_test.go
@@ -1,38 +1,11 @@
 package main
 
-import (
-	"sort"
-	"testing"
-)
-
-var npmMapping = []m{
-	{
+var npmMapping = map[string]m{
+	"Grafana data": {
 		input: "file://dist/tag/grafana_10.2.0-pre_WxeEXjDuHTPB_linux_arm64/npm-artifacts/@grafana-data-10.2.0-pre.tgz",
 		output: []string{
 			"artifacts/npm/v10.2.0-pre/npm-artifacts/@grafana-data-v10.2.0-pre.tgz",
 		},
+		env: map[string]string{"DRONE_TAG": "10.2.0-pre"},
 	},
-}
-
-func TestMoveNPM(t *testing.T) {
-	t.Setenv("DRONE_TAG", "10.2.0-pre")
-	for _, v := range npmMapping {
-		out := NPMHandler(v.input)
-
-		if len(out) != len(v.output) {
-			t.Errorf("expected %d in output but received %d\nexpected: %v\nreceived: %v", len(v.output), len(out), v.output, out)
-			continue
-		}
-		sort.Strings(out)
-		exp := v.output
-		sort.Strings(exp)
-
-		for i := range out {
-			got := out[i]
-			expect := exp[i]
-			if expect != got {
-				t.Errorf("\nExpected %s\nReceived %s", expect, got)
-			}
-		}
-	}
 }
