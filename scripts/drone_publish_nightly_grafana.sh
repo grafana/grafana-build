f2cc524438a2715e7ec0068dc79995b6aa2c81ce
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-09-18 12:55:01 -0300

    Fix filter for storybook and cdn commands

--- scripts/drone_publish_nightly_grafana.sh
+++ scripts/drone_publish_nightly_grafana.sh
@@ -18,12 +18,12 @@ dagger run --silent go run ./cmd package publish \
 
 # Publish only the linux/amd64 edition storybook into the storybook bucket
 dagger run --silent go run ./cmd storybook \
-  $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | awk '{print "--package=file://"$0}') \
+  $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | grep -v docker | awk '{print "--package=file://"$0}') \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} \
   --destination="${STORYBOOK_DESTINATION}/${ver}"
 
 # Publish only the linux/amd64 edition static assets into the static assets bucket
 dagger run --silent go run ./cmd cdn \
-  $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | awk '{print "--package=file://"$0}') \
+  $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | grep -v docker | awk '{print "--package=file://"$0}') \
   --gcp-service-account-key-base64=${GCP_KEY_BASE64} \
   --destination="${CDN_DESTINATION}/${ver}/public"
