89d049fcdf24ed4b70a6438eb383bfb2029d0de3
Guilherme Caulada <guilherme.caulada@grafana.com>
2023-09-25 14:06:51 -0300

    Fix npm publish command

--- scripts/drone_publish_nightly_grafana.sh
+++ scripts/drone_publish_nightly_grafana.sh
@@ -38,7 +38,7 @@ dagger run --silent go run ./cmd cdn \
   --destination="${CDN_DESTINATION}/${ver}/public"
 
 # Publish only the linux/amd64 edition npm packages to npm
-dagger run --silent go run ./cmd npm \
+dagger run --silent go run ./cmd npm publish \
   $(find $local_dir | grep tar.gz | grep linux | grep amd64 | grep -v sha256 | grep -v docker | awk '{print "--package=file://"$0}') \
   --token=${NPM_TOKEN} \
   --tag="nightly"
