71af960f3d4b057cd32687400c3b0a67fe6451fb
Dimitris Sotirakis <dimitrios.sotirakis@grafana.com>
2023-09-28 13:57:40 +0300

    Tarballs: Check if version contains build ID (#187)

--- pipelines/package_names.go
+++ pipelines/package_names.go
@@ -51,7 +51,12 @@ func TarFilename(opts TarFileOpts) string {
 		arch = strings.Join([]string{arch, archv}, "-")
 	}
 
-	p := []string{name, opts.Version, opts.BuildID, os, arch}
+	var p []string
+	if strings.Contains(opts.Version, opts.BuildID) {
+		p = []string{name, opts.Version, os, arch}
+	} else {
+		p = []string{name, opts.Version, opts.BuildID, os, arch}
+	}
 
 	return fmt.Sprintf("%s.tar.gz", strings.Join(p, "_"))
 }
