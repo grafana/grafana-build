6b1befa2550f9174c1253bc54a5758b2c73020cc
Kevin Minehart <kmineh0151@gmail.com>
2023-10-31 17:24:31 -0500

    Fix bug with storybook builds

--- frontend/builder.go
+++ frontend/builder.go
@@ -2,7 +2,6 @@ package frontend
 
 import (
 	"dagger.io/dagger"
-	"github.com/grafana/grafana-build/containers"
 )
 
 // Builder mounts all of the necessary files to run yarn build commands and includes a yarn install exec
@@ -10,22 +9,25 @@ func Builder(d *dagger.Client, platform dagger.Platform, src *dagger.Directory, 
 	container := WithYarnCache(
 		NodeContainer(d, NodeImage(nodeVersion), platform),
 		cache,
-	).WithWorkdir("/src")
+	).WithMountedDirectory("/src", src).WithWorkdir("/src")
 
-	container = containers.WithDirectories(container, map[string]*dagger.Directory{
-		".yarn":           src.Directory(".yarn"),
-		"packages":        src.Directory("packages"),
-		"plugins-bundled": src.Directory("plugins-bundled"),
-		"public":          src.Directory("public"),
-		"scripts":         src.Directory("scripts"),
-	})
+	// TODO: Should figure out exactly what we can include without all the extras so we can take advantage of caching better.
+	// This had to be commented because storybook builds on branches older than 10.1.x were failing.
 
-	container = containers.WithFiles(container, map[string]*dagger.File{
-		"package.json": src.File("package.json"),
-		"lerna.json":   src.File("lerna.json"),
-		"yarn.lock":    src.File("yarn.lock"),
-		".yarnrc.yml":  src.File(".yarnrc.yml"),
-	})
+	// container = containers.WithDirectories(container, map[string]*dagger.Directory{
+	// 	".yarn":           src.Directory(".yarn"),
+	// 	"packages":        src.Directory("packages"),
+	// 	"plugins-bundled": src.Directory("plugins-bundled"),
+	// 	"public":          src.Directory("public"),
+	// 	"scripts":         src.Directory("scripts"),
+	// })
+
+	// container = containers.WithFiles(container, map[string]*dagger.File{
+	// 	"package.json": src.File("package.json"),
+	// 	"lerna.json":   src.File("lerna.json"),
+	// 	"yarn.lock":    src.File("yarn.lock"),
+	// 	".yarnrc.yml":  src.File(".yarnrc.yml"),
+	// })
 
 	// This yarn install is ran just to rebuild the yarn pnp files; all of the dependencies should be in the cache by now
 	return container.WithExec([]string{"yarn", "install", "--immutable"})
