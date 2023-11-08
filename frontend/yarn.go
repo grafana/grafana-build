93f6113f4c17a56919487aeedee6cc6351f600fb
Horst Gutmann <horst.gutmann@grafana.com>
2023-11-08 19:03:36 +0100

    fix(yarn): Fix dependencies on YarnInstall (#218)

--- frontend/yarn.go
+++ frontend/yarn.go
@@ -3,8 +3,8 @@ package frontend
 import "dagger.io/dagger"
 
 func YarnInstall(c *dagger.Client, src *dagger.Directory, version string, cache *dagger.CacheVolume) *dagger.Container {
-	return WithYarnCache(c.Container().From(NodeImage(version)), cache).
+	return WithYarnCache(NodeContainer(c, NodeImage(version), ""), cache).
 		WithMountedDirectory("/src", src).
 		WithWorkdir("/src").
-		WithExec([]string{"yarn", "install", "--immutable"})
+		WithExec([]string{"yarn", "install", "--immutable", "--inline-builds"})
 }
