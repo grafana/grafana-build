77b7734a2cd4ce0a086c04ab0ee04373db3bd1e9
aangelisc <aangelisc@gmail.com>
2024-02-26 18:19:03 +0000

    Src is already a directory

--- backend/builder.go
+++ backend/builder.go
@@ -150,7 +150,7 @@ func Builder(
 	commitInfo := GetVCSInfo(d, src, version, opts.Enterprise)
 
 	builder = withCue(builder, src).
-		WithDirectory("/src/", src.Directory(""), dagger.ContainerWithDirectoryOpts{
+		WithDirectory("/src/", src, dagger.ContainerWithDirectoryOpts{
 			Include: []string{"**/*.mod", "**/*.sum", "**/*.work"},
 		}).
 		WithDirectory("/src/pkg", src.Directory("pkg")).
@@ -175,7 +175,7 @@ func Wire(d *dagger.Client, src *dagger.Directory, platform dagger.Platform, goV
 	// withCue is only required during `make gen-go` in 9.5.x or older.
 	return withCue(golang.Container(d, platform, goVersion), src).
 		WithExec([]string{"apk", "add", "make"}).
-		WithDirectory("/src/", src.Directory(""), dagger.ContainerWithDirectoryOpts{
+		WithDirectory("/src/", src, dagger.ContainerWithDirectoryOpts{
 			Include: []string{"**/*.mod", "**/*.sum", "**/*.work"},
 		}).
 		WithDirectory("/src/pkg", src.Directory("pkg")).
