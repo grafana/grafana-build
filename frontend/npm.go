8699d0eb543ac8370173ffcd06cb880a059b6154
Jack Westbrook <jack.westbrook@gmail.com>
2024-03-28 18:36:02 +0100

    feat(npm): introduce logic to version npm packages using lerna or nx

--- frontend/npm.go
+++ frontend/npm.go
@@ -3,19 +3,49 @@ package frontend
 import (
 	"context"
 	"fmt"
+	"log/slog"
 
 	"dagger.io/dagger"
+	"github.com/Masterminds/semver"
 	"github.com/grafana/grafana-build/containers"
 )
 
 // NPMPackages returns a dagger.Directory which contains the Grafana NPM packages from the grafana source code.
-func NPMPackages(builder *dagger.Container, src *dagger.Directory, ersion string) *dagger.Directory {
+func NPMPackages(ctx context.Context, builder *dagger.Container, d *dagger.Client, log *slog.Logger, src *dagger.Directory, ersion string) (*dagger.Directory, error) {
+	lerna, err := containers.GetJSONValue(ctx, d, src, "package.json", "devDependencies.lerna")
+	if err != nil {
+		log.Error("Tried to read lerna version from package.json but failed.", "error", err)
+		return nil, err
+	}
+
+	_, err = semver.NewVersion(lerna)
+	if err != nil {
+		nx, err := containers.GetJSONValue(ctx, d, src, "package.json", "devDependencies.nx")
+		if err != nil {
+			log.Error("Tried to read nx version from package.json but failed.", "error", err)
+			return nil, err
+		}
+
+		_, err = semver.NewVersion(nx)
+		if err != nil {
+			log.Error("Could not find a valid version for either lerna or nx in package.json.", "error", err)
+			return nil, err
+		}
+
+		// No lerna version specified in package.json
+		return builder.WithExec([]string{"mkdir", "npm-packages"}).
+			WithExec([]string{"yarn", "packages:build"}).
+			WithExec([]string{"yarn", "nx", "release", "version", ersion, "--no-git-tag", "--no-git-commit", "--group", "grafanaPackages"}).
+			// TODO: Jack - figure out how to do this with nx.
+			// WithExec([]string{"yarn", "lerna", "exec", "--no-private", "--", "yarn", "pack", "--out", fmt.Sprintf("/src/npm-packages/%%s-%v.tgz", "v"+ersion)}).
+			Directory("./npm-packages"), nil
+	}
+
 	return builder.WithExec([]string{"mkdir", "npm-packages"}).
-		WithExec([]string{"yarn", "run", "packages:build"}).
-		// TODO: We should probably start reusing the yarn pnp map if we can figure that out instead of rerunning yarn install everywhere.
-		WithExec([]string{"yarn", "run", "lerna", "version", ersion, "--exact", "--no-git-tag-version", "--no-push", "--force-publish", "-y"}).
+		WithExec([]string{"yarn", "packages:build"}).
+		WithExec([]string{"yarn", "lerna", "version", ersion, "--exact", "--no-git-tag-version", "--no-push", "--force-publish", "-y"}).
 		WithExec([]string{"yarn", "lerna", "exec", "--no-private", "--", "yarn", "pack", "--out", fmt.Sprintf("/src/npm-packages/%%s-%v.tgz", "v"+ersion)}).
-		Directory("./npm-packages")
+		Directory("./npm-packages"), nil
 }
 
 // PublishNPM publishes a npm package to the given destination.
