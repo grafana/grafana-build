b1e6cc3bf1f883a2495330b6670d5fbfb843d392
Jack Westbrook <jack.westbrook@gmail.com>
2024-04-12 14:36:16 +0200

    refactor(daggerutil): update fileexists to print errors

--- daggerutil/fileexists.go
+++ daggerutil/fileexists.go
@@ -2,15 +2,17 @@ package daggerutil
 
 import (
 	"context"
+	"fmt"
 
 	"dagger.io/dagger"
 )
 
 func FileExists(ctx context.Context, dir *dagger.Directory, path string) bool {
 	_, err := dir.File(path).Contents(ctx)
-	if err == nil {
-		return true
+	if err != nil {
+		fmt.Printf("error checking if file exists: %s", err.Error())
+		return false
 	}
 
-	return false
+	return true
 }
