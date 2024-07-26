bdc0d546d930efce47f38f596e4da36c922eb41e
Kevin Minehart <5140827+kminehart@users.noreply.github.com>
2024-07-26 14:04:11 -0500

    add patches-path and patches-repo to drone_build_tag_pro and enterpri… (#344)

--- scripts/drone_build_tag_enterprise.sh
+++ scripts/drone_build_tag_enterprise.sh
@@ -42,6 +42,8 @@ dagger run go run ./cmd \
   --grafana-repo=https://github.com/grafana/grafana-security-mirror.git \
   --github-token=${GITHUB_TOKEN} \
   --go-version=${GO_VERSION} \
+  --patches-repo=${PATCHES_REPO} \
+  --patches-path=${PATCHES_PATH} \
   --ubuntu-base="${UBUNTU_BASE}" \
   --alpine-base="${ALPINE_BASE}" \
   --version=${DRONE_TAG} \
